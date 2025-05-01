cfd3f8b6b884fc0f84d89d313d6f4c421a9e5725
hkereb <115366640+hkereb@users.noreply.github.com>
2025-05-01 15:42:29 +0200

    change concept to pharmacy

--- lab5/src/modules/products/model.js
+++ lab5/src/modules/products/model.js
@@ -4,6 +4,12 @@ const db = require("../../db");
 exports.getAllProducts = async () => {
   try {
     const [rows] = await db.query("SELECT * FROM products");
+
+    // Konwertowanie ceny na liczbę, jeśli nie jest już liczbą
+    rows.forEach(product => {
+        product.price = parseFloat(product.price);  // Upewniamy się, że price to liczba
+    });
+
     return rows; // Zwrócenie wyników
   } catch (err) {
     console.error("Error fetching products:", err);
